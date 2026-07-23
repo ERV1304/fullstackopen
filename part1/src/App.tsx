7d52eb0cd4831fc348bbd88831b9c885a9c582b7
ERV1304 <ernesto_r_v@hotmail.com>
2026-07-23 15:06:05 +0200

    Add  JS concepts to basic React example

--- part1/src/App.tsx
+++ part1/src/App.tsx
@@ -17,6 +17,47 @@ function Hello(props: {name?: string, age?: number }) {
   )
 }
 
+function Javascript() {
+  const t = [1, -1, 3] // define un array de números
+  console.log(t)  // se imprime el array [1, -1, 3]
+
+  t.forEach(value => {
+    console.log(value)  // se imprimen los números 1,-1,3 cada uno en su propia línea
+  })
+
+  const t2 = t.concat(5) // crea un nuevo array con el valor 5 agregado al final
+  console.log(t2) // se imprime el array [1, -1, 3, 5]
+
+  const m1 = t.map(value => value * 2)
+  console.log(m1) // se imprime el array [2, -2, 6]
+
+  const m2 = t.map(value => '<li>' + value + '</li>')
+  console.log(m2) // se imprime el array [ '<li>1</li>', '<li>-1</li>', '<li>3</li>' ]
+
+  const [first, second, ...rest] = t2
+  console.log(first, second)  // se imprime 1 -1
+  console.log(rest)  // se imprime el array [3, 5]
+
+  const sum = (p1: number, p2: number) => { 
+    console.log(p1) // se imprime 1 
+    console.log(p2) // se imprime 5 
+    return p1 + p2 
+  } 
+  const result = sum(1, 5)
+  console.log (result) // se imprime 6
+
+  const tSquared = t.map(p => p * p)
+  console.log (tSquared) // se imprime el array [1, 1, 9]
+
+  const average = function(a: number, b: number) {
+  return (a + b) / 2
+  }
+  const result2 = average(10, 20)
+  console.log(result2) // se imprime 15
+
+  return (<></>)
+}
+
 function App() {
   const [count, setCount] = useState(0)
   console.log('Hello from Main APP')
@@ -137,6 +178,10 @@ function App() {
       <section id="right-side">
         <Hello name="ERV" age={a+b} />
         <div>{friends}</div>
+      </section>
+
+      <section id="javascript">
+        <Javascript />
       </section>
 
       <div className="ticks"></div>
