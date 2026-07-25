2182aa23592351a9418ed2a9460d6f0f2457d6de
ERV1304 <ernesto_r_v@hotmail.com>
2026-07-25 08:03:52 +0200

    Part 1 - Exercise 1.7

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -13,7 +13,10 @@ function Header(props: {name?: string}) {
 function Display({level, count}: {level: string, count: number}) {
   return (
     <>
-      <p>{level} {count}</p>
+      <p>
+        {level} {count}
+        {level === 'Percentage' && ' %'}
+      </p>
     </>
   )
 }
@@ -33,6 +36,7 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleClick = ({level}: {level: string}) => {
     if (level === 'good') {
@@ -42,18 +46,28 @@ function App() {
     } else if (level === 'bad') {
       setBad(bad + 1)
     }
+    setAll(all + 1)
   }
 
+  const average = () => all > 0 ? (good * 1 + neutral * 0 + bad * -1) / all : 0
+
+  const percentage = () => all > 0 ? (good / all) * 100 : 0
+
   return (
-    <section id="left">
+    <section id="central">
       <Header name={'give feedback'} />
       <Button onClick={() => handleClick({level: 'good'})} text="good" />
       <Button onClick={() => handleClick({level: 'neutral'})} text="neutral" />
       <Button onClick={() => handleClick({level: 'bad'})} text="bad" />
       <Header name={'statistics'} />
-      <Display level="Good" count={good} />
-      <Display level="Neutral" count={neutral} />
-      <Display level="Bad" count={bad} />
+      <section id="left">
+        <Display level="Good" count={good} />
+        <Display level="Neutral" count={neutral} />
+        <Display level="Bad" count={bad} />
+        <Display level="All" count={all} />
+        <Display level="Average" count={average()} />
+        <Display level="Percentage" count={percentage()} />
+      </section>
     </section>
   )
 }
