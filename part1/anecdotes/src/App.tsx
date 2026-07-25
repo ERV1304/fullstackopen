965b84f229b63d345bca0274cc92aed5562e2c23
ERV1304 <ernesto_r_v@hotmail.com>
2026-07-25 21:01:06 +0200

    Part 1 - Exercise14 - Temp Version

--- part1/anecdotes/src/App.tsx
+++ part1/anecdotes/src/App.tsx
@@ -3,6 +3,34 @@ import './App.css'
 
 const RandomInit = (length: number) => Math.floor(Math.random() * length)
 
+function Header(props: {name?: string}) {
+  
+  return (
+    <>
+     <h1>Anecdote {props.name}</h1>
+    </>
+  )
+}
+
+function Display({text}: {text: string}) {
+  return (
+    <>
+      <p>{text}</p>
+    </>
+  )
+}
+
+function Button(props: {onClick: () => void, text: string}) {
+
+  return (
+    <>
+      <button className="button" onClick={props.onClick}>
+        {props.text}
+      </button>
+    </>
+  )
+}
+
 function App() {
    const anecdotes = [
     'If it hurts, do it more often.',
@@ -11,18 +39,31 @@ function App() {
     'Premature optimization is the root of all evil.',
     'The only way to go fast, is to go well.'
   ]
-  const [, setSelected] = useState(0)
+  const [selected, setSelected] = useState(RandomInit(anecdotes.length))
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const [mostVoted, setMostVoted] = useState(0)
 
-  const initRandom = RandomInit(anecdotes.length)
+  const handleVotes = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+    setMostVoted(votes.indexOf(Math.max(...copy)))
+  }
+
+  console.log('Votes:', votes)
 
   return (
     <>
-      <div>
-        <p>{anecdotes[initRandom]}</p>
-        <button onClick={() => setSelected(RandomInit(anecdotes.length))}>
-          Next Anecdote
-        </button>
-      </div>
+        <section id="central">
+        <Header name={'of the day'} />
+        <Display text={anecdotes[selected]} />
+        <Display text={`has ${votes[selected]} votes`} />
+        <Button onClick={handleVotes} text="Vote" />
+        <Button onClick={() => setSelected(RandomInit(anecdotes.length))} text="Next Anecdote" />
+        <Header name={'with most votes'} />
+        <Display text={anecdotes[mostVoted]} />
+        <Display text={`has ${votes[mostVoted]} votes`} />
+      </section>
     </>
   )
 }
